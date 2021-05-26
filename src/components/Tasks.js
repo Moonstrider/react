d2955e2475684340dbc647bc01d847c987c4ac32
David Zhu <37094943+Moonstrider@users.noreply.github.com>
2021-05-26 10:58:01 +0800

    123

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -4,9 +4,13 @@ const Tasks = ({ tasks, onDelete, onToggle }) => {
     return (
         <>
             {tasks.map((task, index) => (
-                <Task key={index}
+                <Task
+                    key={index}
+
                     task={task}
+
                     onDelete={onDelete}
+
                     onToggle={onToggle} />
             ))}
         </>
